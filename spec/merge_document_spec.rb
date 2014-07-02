22a074989861f26055343d9a67be73d5795d414d
Hajime Sueyoshi <hajime.sueyoshi@gaiax.com>
2014-07-02 19:01:14 +0900

    add test case

--- spec/merge_document_spec.rb
+++ spec/merge_document_spec.rb
@@ -36,5 +36,70 @@ describe "Merge document" do
 
       it { is_expected.to eq(expected) }
     end
+
+    context "when documents has NOT same key" do
+      let(:docs) do
+        [
+          { "key" => "elmar", "data" => { "length" => "35", "aperture" => "3.5" } },
+          { "key" => "summicron", "data" => { "year" => "1963" } },
+        ]
+      end
+
+      let(:expected) do
+        [
+          { "key" => "elmar", "data" => { "length" => "35", "aperture" => "3.5" } },
+          { "key" => "summicron", "data" => { "year" => "1963" } },
+        ]
+      end
+
+      it { is_expected.to eq(expected) }
+    end
+  end
+
+  context "given 3 different schema document" do
+    context "when documents has same key" do
+      let(:docs) do
+        [
+          { "key" => "summicron", "data" => { "length" => "50", "aperture" => "2" } },
+          { "key" => "elmar", "data" => { "length" => "35", "aperture" => "3.5" } },
+          { "key" => "elmar", "data" => { "year" => "1954" } },
+        ]
+      end
+
+      let(:expected) do
+        [
+          {
+            "key" => "summicron",
+            "data" => { "length" => "50", "aperture" => "2" }
+          },
+          {
+            "key" => "elmar",
+            "data" => { "length" => "35", "aperture" => "3.5", "year" => "1954" }
+          }
+        ]
+      end
+
+      it { is_expected.to eq(expected) }
+    end
+
+    context "when documents has NOT same key" do
+      let(:docs) do
+        [
+          { "key" => "elmar", "data" => { "length" => "35", "aperture" => "3.5" } },
+          { "key" => "summicron", "data" => { "year" => "1963" } },
+          { "key" => "noctilux", "data" => { "length" => "50", "aperture" => "1.1" } },
+        ]
+      end
+
+      let(:expected) do
+        [
+          { "key" => "elmar", "data" => { "length" => "35", "aperture" => "3.5" } },
+          { "key" => "summicron", "data" => { "year" => "1963" } },
+          { "key" => "noctilux", "data" => { "length" => "50", "aperture" => "1.1" } },
+        ]
+      end
+
+      it { is_expected.to eq(expected) }
+    end
   end
 end
