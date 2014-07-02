ad6d19c7b40b3b2b9874059fbf84f66aca607be6
Hajime Sueyoshi <hajime.sueyoshi@gaiax.com>
2014-07-02 18:41:41 +0900

    pass options for document keys

--- lib/document_merger.rb
+++ lib/document_merger.rb
@@ -1,22 +1,22 @@
 class DocumentMerger
 
-  def self.merge(documents)
-    new(documents).merge
+  def self.merge(documents, *options)
+    new(documents, *options).merge
   end
 
-  def initialize(documents)
+  def initialize(documents, merge_key: "key", data_key: "data")
     @documents = documents
+    @merge_key = merge_key
+    @data_key = data_key
   end
 
   def merge
     return @documents if @documents.size == 1
-    group = @documents.group_by {|d| d["key"] }
+    group = @documents.group_by {|d| d[@merge_key] }
 
-    result = []
-    group.each do |k, v|
-      new_data = v.inject({}) {|r, e| r.merge(e["data"]) }
-      result << { "key" => k, "data" => new_data }
+    group.keys.inject([]) do |result, k|
+      merged_data = group[k].inject({}) {|r, e| r.merge(e[@data_key]) }
+      result << { @merge_key => k, @data_key => merged_data }
     end
-    result
   end
 end
