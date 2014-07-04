0547737d244e96867be8f7398fa412c346519cd7
Hajime Sueyoshi <hajime.sueyoshi@gaiax.com>
2014-07-04 12:13:03 +0900

    change merge strategy

--- lib/document_merger.rb
+++ lib/document_merger.rb
@@ -1,18 +1,3 @@
-require 'hashie'
-
-class Document < Hash
-  include Hashie::Extensions::DeepMerge
-
-  alias_method :aggregate, :deep_merge
-
-  class << self
-
-    def parse(hash)
-      new.merge(hash)
-    end
-  end
-end
-
 class DocumentMerger
 
   class << self
@@ -20,27 +5,37 @@ class DocumentMerger
     def merge(documents, *options)
       new(documents, *options).merge
     end
-
-    def create_document(hashes)
-      hashes.map {|h| Document.parse(h) }
-    end
   end
 
-  def initialize(hashes, merge_key: "key", data_key: "data")
-    @documents = self.class.create_document(hashes)
-    @merge_key = merge_key
-    @data_key = data_key
+  def initialize(documents, key: "key")
+    @documents = documents
+    @merge_key = key
   end
 
   def merge
     return @documents if @documents.size == 1
 
-    groups = @documents.group_by {|d| d[@merge_key] }
-    groups.keys.inject([]) do |result, group_key|
-      group = groups[group_key]
-      if group.size <= 2
-        result << group[0].aggregate(group[1])
+    merged = []
+    @documents.group_by {|d| d[@merge_key] }.each do |key, origin|
+      padded = []
+
+      schema_sets = origin.group_by {|o| o.keys.hash }
+
+      max_size_of_sets = schema_sets.values.map {|v| v.size }.max
+
+      schema_sets.values.each do |schema_set|
+        until schema_set.size == max_size_of_sets
+          schema_set << schema_set.first
+        end
+        padded << schema_set
+      end
+
+      padded.inject(&:zip).each do |same_key_docs|
+        merged_doc = [same_key_docs].flatten.inject(&:merge)
+        merged << merged_doc
       end
     end
+
+    merged
   end
 end
--- spec/merge_document_spec.rb
+++ spec/merge_document_spec.rb
@@ -1,130 +1,248 @@
 require 'spec_helper'
 require 'document_merger'
 
-describe "Documentのマージ" do
+describe "HashのArrayのマージ" do
   subject do
-    DocumentMerger.merge(docs)
+    DocumentMerger.merge(docs, key: "name")
   end
 
-  context "Documentが1つの場合" do
+  context "Hashが1つの場合" do
     let(:docs) do
       [
-        { "key" => "elmar", "data" => { "length" => "35", "aperture" => "3.5" } }
+        { "name" => "elmar", "length" => "35", "aperture" => "3.5" }
       ]
     end
 
-    it("そのまま") { is_expected.to eq(docs) }
+    it("そのまま") { is_expected.to match_array(docs) }
   end
 
-  context "2種類のDocumentが1件ずつある" do
-    context "結合キーが同じ" do
-      let(:docs) do
-        [
-          { "key" => "elmar", "data" => { "length" => "35", "aperture" => "3.5" } },
-          { "key" => "elmar", "data" => { "year" => "1954" } },
-        ]
+  context "Hashが2種類の場合" do
+    context "HashA,HashBが1件ずつ" do
+      context "結合キーの値が同じ" do
+        let(:docs) do
+          [
+            { "name" => "elmar", "length" => "35", "aperture" => "3.5" },
+            { "name" => "elmar", "year" => "1954" },
+          ]
+        end
+
+        let(:expected) do
+          [
+            { "name" => "elmar", "length" => "35", "aperture" => "3.5", "year" => "1954" }
+          ]
+        end
+
+        it("結合する") { is_expected.to match_array(expected) }
       end
 
-      let(:expected) do
-        [
-          {
-            "key" => "elmar",
-            "data" => { "length" => "35", "aperture" => "3.5", "year" => "1954" }
-          }
-        ]
-      end
+      context "結合キーの値が違う" do
+        let(:docs) do
+          [
+            { "name" => "elmar", "length" => "35", "aperture" => "3.5" },
+            { "name" => "summicron", "year" => "1963" },
+          ]
+        end
 
-      it("結合する") { is_expected.to eq(expected) }
+        let(:expected) do
+          [
+            { "name" => "elmar", "length" => "35", "aperture" => "3.5" },
+            { "name" => "summicron", "year" => "1963" },
+          ]
+        end
+
+        it("結合しない") { is_expected.to match_array(expected) }
+      end
     end
 
-    context "結合キーが違う" do
-      let(:docs) do
-        [
-          { "key" => "elmar", "data" => { "length" => "35", "aperture" => "3.5" } },
-          { "key" => "summicron", "data" => { "year" => "1963" } },
-        ]
+    context "HashAが2件,HashBが1件の場合" do
+      context "HashAとHashBで結合キーが同じ値の組が1つ" do
+        let(:docs) do
+          [
+            { "name" => "summicron", "length" => "50", "aperture" => "2" },
+            { "name" => "elmar", "length" => "35", "aperture" => "3.5" },
+            { "name" => "elmar", "year" => "1954" },
+          ]
+        end
+
+        let(:expected) do
+          [
+            { "name" => "summicron", "length" => "50", "aperture" => "2" },
+            { "name" => "elmar", "length" => "35", "aperture" => "3.5", "year" => "1954" }
+          ]
+        end
+
+        it("1組結合する") { is_expected.to match_array(expected) }
       end
 
-      let(:expected) do
-        [
-          { "key" => "elmar", "data" => { "length" => "35", "aperture" => "3.5" } },
-          { "key" => "summicron", "data" => { "year" => "1963" } },
-        ]
+      context "全て結合キーの値が同じ" do
+        let(:docs) do
+          [
+            { "name" => "summicron", "length" => "50", "aperture" => "2" },
+            { "name" => "summicron", "length" => "35", "aperture" => "2" },
+            { "name" => "summicron", "year" => "1963" },
+          ]
+        end
+
+        let(:expected) do
+          [
+            { "name" => "summicron", "length" => "35", "aperture" => "2", "year" => "1963" },
+            { "name" => "summicron", "length" => "50", "aperture" => "2", "year" => "1963" }
+          ]
+        end
+
+        it("HashBを2件に増やして結合する") { is_expected.to match_array(expected) }
       end
 
-      it("結合しない") { is_expected.to eq(expected) }
+      context "結合キーの値が全て違う" do
+        let(:docs) do
+          [
+            { "name" => "elmar", "length" => "35", "aperture" => "3.5" },
+            { "name" => "summicron", "year" => "1963" },
+            { "name" => "noctilux", "length" => "50", "aperture" => "1.1" },
+          ]
+        end
+
+        let(:expected) do
+          [
+            { "name" => "elmar", "length" => "35", "aperture" => "3.5" },
+            { "name" => "summicron", "year" => "1963" },
+            { "name" => "noctilux", "length" => "50", "aperture" => "1.1" },
+          ]
+        end
+
+        it("結合しない") { is_expected.to match_array(expected) }
+      end
     end
   end
 
-  context "2種類のDocumentが3件以上ある" do
-    context "結合キーが同じDocumentが2件ある" do
-      let(:docs) do
-        [
-          { "key" => "summicron", "data" => { "length" => "50", "aperture" => "2" } },
-          { "key" => "elmar", "data" => { "length" => "35", "aperture" => "3.5" } },
-          { "key" => "elmar", "data" => { "year" => "1954" } },
-        ]
+  context "Hashが3種類の場合" do
+    context "HashA,HashB,HashCが1件ずつ" do
+      context "結合キーの値が同じ" do
+        let(:docs) do
+          [
+            { "name" => "elmar", "length" => "35", "aperture" => "3.5" },
+            { "name" => "elmar", "year" => "1954" },
+            { "name" => "elmar", "new_price" => "500", "old_price" => "300" },
+          ]
+        end
+
+        let(:expected) do
+          [
+            {
+              "name" => "elmar",
+              "length" => "35", "aperture" => "3.5",
+              "year" => "1954",
+              "new_price" => "500", "old_price" => "300"
+            }
+          ]
+        end
+
+        it("結合する") { is_expected.to match_array(expected) }
       end
 
-      let(:expected) do
-        [
-          {
-            "key" => "summicron",
-            "data" => { "length" => "50", "aperture" => "2" }
-          },
-          {
-            "key" => "elmar",
-            "data" => { "length" => "35", "aperture" => "3.5", "year" => "1954" }
-          }
-        ]
-      end
+      context "結合キーの値が違う" do
+        let(:docs) do
+          [
+            { "name" => "elmar", "length" => "35", "aperture" => "3.5" },
+            { "name" => "summicron", "year" => "1963" },
+            { "name" => "summaron", "new_price" => "700", "old_price" => "550" },
+          ]
+        end
 
-      it("結合する") { is_expected.to eq(expected) }
+        let(:expected) do
+          [
+            { "name" => "elmar", "length" => "35", "aperture" => "3.5" },
+            { "name" => "summicron", "year" => "1963" },
+            { "name" => "summaron", "new_price" => "700", "old_price" => "550" },
+          ]
+        end
+
+        it("結合しない") { is_expected.to match_array(expected) }
+      end
     end
 
-    context "結合キーが同じDocumentが3件ある" do
-      let(:docs) do
-        [
-          { "key" => "summicron", "data" => { "length" => "50", "aperture" => "2" } },
-          { "key" => "summicron", "data" => { "length" => "35", "aperture" => "2" } },
-          { "key" => "summicron", "data" => { "year" => "1963" } },
-        ]
+    context "HashAが2件,HashBが1件,HashCが2件の場合" do
+      context "HashAとHashBとHashCでで結合キーが同じ値の組が1つ" do
+        let(:docs) do
+          [
+            { "name" => "summicron", "length" => "50", "aperture" => "2" },
+            { "name" => "elmar", "length" => "35", "aperture" => "3.5" },
+            { "name" => "elmar", "year" => "1954" },
+            { "name" => "elmar", "new_price" => "700", "old_price" => "500" },
+            { "name" => "summilux", "new_price" => "1800", "old_price" => "1100" },
+          ]
+        end
+
+        let(:expected) do
+          [
+            { "name" => "summicron", "length" => "50", "aperture" => "2" },
+            {
+              "name" => "elmar",
+              "length" => "35", "aperture" => "3.5",
+              "year" => "1954",
+              "new_price" => "700", "old_price" => "500"
+            },
+            { "name" => "summilux", "new_price" => "1800", "old_price" => "1100" }
+          ]
+        end
+
+        it("1組結合する") { is_expected.to match_array(expected) }
       end
 
-      let(:expected) do
-        [
-          {
-            "key" => "summicron",
-            "data" => { "length" => "35", "aperture" => "2", "year" => "1963" }
-          },
-          {
-            "key" => "summicron",
-            "data" => { "length" => "50", "aperture" => "2", "year" => "1963" }
-          }
-        ]
+      context "全て結合キーの値が同じ" do
+        let(:docs) do
+          [
+            { "name" => "summicron", "length" => "50", "aperture" => "2" },
+            { "name" => "summicron", "length" => "35", "aperture" => "2" },
+            { "name" => "summicron", "year" => "1963" },
+            { "name" => "summicron", "new_price" => "1000", "old_price" => "800" },
+            { "name" => "summicron", "new_price" => "2300", "old_price" => "1500" },
+          ]
+        end
+
+        let(:expected) do
+          [
+            {
+              "name" => "summicron",
+              "length" => "50", "aperture" => "2",
+              "year" => "1963",
+              "new_price" => "1000", "old_price" => "800"
+            },
+            {
+              "name" => "summicron",
+              "length" => "35", "aperture" => "2",
+              "year" => "1963",
+              "new_price" => "2300", "old_price" => "1500"
+            }
+          ]
+        end
+
+        it("HashBを2件に増やして2組結合する") { is_expected.to match_array(expected) }
       end
 
-      it("件数が多い種類に合わせて結合する") { is_expected.to eq(expected) }
-    end
+      context "結合キーの値が全て違う" do
+        let(:docs) do
+          [
+            { "name" => "elmar", "length" => "35", "aperture" => "3.5" },
+            { "name" => "summicron", "year" => "1963" },
+            { "name" => "noctilux", "length" => "50", "aperture" => "1.1" },
+            { "name" => "super-angulon", "new_price" => "1300", "old_price" => "1100" },
+            { "name" => "hektor", "new_price" => "1400", "old_price" => "900" },
+          ]
+        end
 
-    context "結合キーが全て違う" do
-      let(:docs) do
-        [
-          { "key" => "elmar", "data" => { "length" => "35", "aperture" => "3.5" } },
-          { "key" => "summicron", "data" => { "year" => "1963" } },
-          { "key" => "noctilux", "data" => { "length" => "50", "aperture" => "1.1" } },
-        ]
+        let(:expected) do
+          [
+            { "name" => "elmar", "length" => "35", "aperture" => "3.5" },
+            { "name" => "summicron", "year" => "1963" },
+            { "name" => "noctilux", "length" => "50", "aperture" => "1.1" },
+            { "name" => "super-angulon", "new_price" => "1300", "old_price" => "1100" },
+            { "name" => "hektor", "new_price" => "1400", "old_price" => "900" },
+          ]
+        end
+
+        it("結合しない") { is_expected.to match_array(expected) }
       end
-
-      let(:expected) do
-        [
-          { "key" => "elmar", "data" => { "length" => "35", "aperture" => "3.5" } },
-          { "key" => "summicron", "data" => { "year" => "1963" } },
-          { "key" => "noctilux", "data" => { "length" => "50", "aperture" => "1.1" } },
-        ]
-      end
-
-      it("結合しない") { is_expected.to eq(expected) }
     end
   end
 end
